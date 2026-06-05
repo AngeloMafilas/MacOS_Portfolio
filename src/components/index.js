b974bb070dd15f7cbd4d9e86c60467deed3809dd
AngeloMafilas <angelomafilas33@gmail.com>
2026-06-05 15:58:47 +0400

    fix: remove unused WindowController import/export causing build failures

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
 import Dock from "./Dock";
-import WindowController from "./WindowController";
 
-export { Navbar, Welcome, Dock, WindowController};
+export { Navbar, Welcome, Dock };
